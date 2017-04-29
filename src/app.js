e834924b1997372e0c60dc8e4ce415f37cf9990b
Leo Germani <leogermani@hacklab.com.br>
2017-04-29 19:02:17 -0300

    teste com pins, novo estilo e paineis

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
 $(function() {
 
     
-    var map = L.map('mapid').setView([51.505, -0.09], 2);
+    var map = L.map('mapid', {
+        zoomControl: false,
+        center: [51.505, -0.09],
+        zoom: 2
+    });
     
     
+    $('.close-thik').click(function() {
+        $(this).parent().removeClass('on');
+        if ($(this).parent().attr('id') == 'info')
+            $('#info-button').show();
+    });
+    
+    $('#info-button').click(function() {
+        $(this).hide();
+        $('#country-info .close-thik').click();
+        $('#info').addClass('on');
+    });
     
     
     // load geojson
@@ -11,7 +26,7 @@ $(function() {
     
         var countries = countries;
         
-        $.getJSON('info.json', function(info) {
+        $.getJSON('info.json?asd', function(info) {
             
             
             //console.log(info);
@@ -20,12 +35,13 @@ $(function() {
                 //console.log(v.id);
                 //console.log(info);
                 if (v.id in info) {
-                
                     countries.features[c].properties.name = info[v.id].name;
                     countries.features[c].properties.scale = info[v.id].scale;
                     countries.features[c].properties.description = info[v.id].description;
                     countries.features[c].properties.list = info[v.id].list;
                     
+                } else {
+                    countries.features[c].properties.scale = 0;
                 }
                 
                 
@@ -37,20 +53,55 @@ $(function() {
             
                 style: function(feature) {
                     
+                    var colors = ['#a2a2a2','#91709d','#8a559d','#71218f','#7a1b9d','#73009d'];
+                    
                     var s = {
-                        weight: 1
+                        weight: 1,
+                        color: 'white',
+                        fillColor: colors[feature.properties.scale],
+                        fillOpacity: 1
                     }
                     
                     
-                    if (feature.properties.scale > 1) {
                     
-                        s.color = 'red';
-                        
-                    }
                     
                     return s;
                     
                 
+                },
+                
+                onEachFeature: function (feature, layer) {
+                    
+                    
+                    
+                    
+                    
+                    if (feature.properties.scale > 1) {
+                    
+                        var c = layer.getBounds().getCenter();
+                        L.marker(c).addTo(map);
+                        
+                        layer.bindTooltip(feature.properties.name);
+                        
+                        layer.on({
+                            'mousemove': function (e) {
+                                layer.openTooltip();
+                            }, 
+                            'mouseout': function (e) {
+                                layer.closeTooltip();
+                            },
+                            'click': function(e) {
+                                $('#info .close-thik').click();
+                                $('#country-info h1').html(feature.properties.name);
+                                $('#country-info .content').html(feature.properties.description);
+                                $('#country-info').addClass('on');
+                            }
+                        });
+                        
+                    }
+                    
+                    
+                    
                 }
             
             }).addTo(map);
